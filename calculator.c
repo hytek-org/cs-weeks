1e2388b62cb90fc60e8de74d6b3889a297a308a6
root <root@172-105-50-44.ip.linodeusercontent.com>
2022-06-17 23:31:58 +0000

    main

--- calculator.c
+++ calculator.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 
-void main()
+int main(void)
 {
 char op;
+printf("Operator[+,-,*,/]::");
+
+scanf("%c",&op);
+
 float a,b,result;
 
 printf("Enter no of A:\n");
 scanf("%f",&a);
 printf("Enter no of B:\n");
-scanf(“%f”,&b);
-printf("Operator[+,-,*,/]::\n");
-fflush(stdin);
-scanf("%c",&op);
-if(op=='+')
+scanf("%f",&b);
+
+
+
+if(op == '+')
 {
 result=a+b;
-printf("Result:>>%.2f",result);
+printf("Result:>>%.2f\n",result);
 }
-else if(op=='*')
+else if(op == '*')
 {
 result=a*b;
-printf("Result:>>%.2f",result);
+printf("Result:>>%.2f\n",result);
 }
-else if(op=='-')
+else if(op == '-')
 {
 result=a-b;
-printf("Result:>>%.2f",result);
+printf("Result:>>%.2f\n",result);
 }
 else if(op == '/'){
 result = a/b;
-printf("Result:>>%.2f",result);
+printf("Result:>>%.2f\n",result);
 }
 else
 {
-printf("Invalid Choice:");
+printf("Invalid Choice:\n");
 }
+
+
+return 0;
 
 }
